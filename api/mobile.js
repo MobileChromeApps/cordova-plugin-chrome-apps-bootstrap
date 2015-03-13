MobileChromeApps/cordova-plugin-chrome-apps-bootstrap

commit f4ce8f62dc9bcacffed3130c6b281fd1024ee050

Make bootstrap CSP-compatible

api/mobile.js
@@ -47,7 +47,7 @@ function restoreWindowOpen(context) {
   delete context.open;
 }
 
-exports.boot = function() {
+function boot() {
   // Add a deviceready listener that initializes the Chrome wrapper.
   channel.onCordovaReady.subscribe(function() {
     // Delay bootstrap until all deviceready event dependancies fire, minus DOMContentLoaded, since that one is purposely being blocked by bootstrap
@@ -167,3 +167,4 @@ function fireLifecycleEvents(manifestJson) {
   });
 }
 
+require('cordova/channel').onPluginsReady.subscribe(boot);

plugin.xml
@@ -6,7 +6,7 @@
 -->
 <plugin xmlns="http://cordova.apache.org/ns/plugins/1.0"
     id="org.chromium.bootstrap"
-    version="1.1.4-dev">
+    version="2.0.0-dev">
   <engines>
     <engine name="cordova" version=">=3.0.0" />
   </engines>
@@ -27,12 +27,14 @@
 
   <js-module src="api/mobile.js" name="mobile.impl">
     <clobbers target="chrome.mobile.impl" />
+    <runs />
   </js-module>
 
   <!-- Integration files -->
   <asset src="www/chromeapp.html" target="plugins/org.chromium.bootstrap/chromeapp.html" />
   <asset src="www/chromebgpage.html" target="plugins/org.chromium.bootstrap/chromebgpage.html" />
   <asset src="www/chromeappstyles.css" target="plugins/org.chromium.bootstrap/chromeappstyles.css" />
+  <asset src="www/bgboot.js" target="plugins/org.chromium.bootstrap/bgboot.js" />
 
   <platform name="android">
     <source-file src="src/android/ChromeAppWindow.java" target-dir="src/org/chromium" />

www/chromeapp.html
@@ -7,14 +7,10 @@ found in the LICENSE file.
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="Content-Security-Policy" content="This gets injected by cca">
         <meta name="format-detection" content="telephone=no">
         <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
         <script src="../../cordova.js"></script>
-        <script>
-            cordova.require('cordova/channel').onPluginsReady.subscribe(function() {
-                cordova.require('org.chromium.bootstrap.mobile.impl').boot();
-            });
-        </script>
     </head>
     <body style="background-color: black;">
         <script src="chrome-extension://z/chrome-content-loaded"></script>

www/bgboot.js
@@ -1,0 +1,4 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+parent.chrome.mobile.impl.bgInit(this);

www/chromebgpage.html
@@ -5,7 +5,10 @@ Use of this source code is governed by a BSD-style license that can be
 found in the LICENSE file.
 -->
 <html>
+    <head>
+        <meta http-equiv="Content-Security-Policy" content="This gets injected by cca">
+    </head>
   <body>
-    <script>parent.chrome.mobile.impl.bgInit(this)</script>
+    <script src="bgboot.js"></script>
   </body>
 </html>

commit e50eb5d78aeb14ec64389822a7070d1912e892bd

Don't call boot() from mobile.js in order to neuter plugin within CADT
This was a regression in f4ce8f62dc

api/mobile.js
@@ -47,14 +47,13 @@ function restoreWindowOpen(context) {
   delete context.open;
 }
 
-function boot() {
+exports.fgInit = function() {
   // Add a deviceready listener that initializes the Chrome wrapper.
   channel.onCordovaReady.subscribe(function() {
     // Delay bootstrap until all deviceready event dependancies fire, minus DOMContentLoaded, since that one is purposely being blocked by bootstrap
     // We do this delay so that plugins have a chance to initialize using the bridge before we load the chrome app background scripts/event page
     var channelsToWaitFor = channel.deviceReadyChannelsArray.filter(function(c) { return c.type !== 'onDOMContentLoaded'; });
     channel.join(function() {
-
       // Undo the clobber of window.open by InAppBrowser
       restoreWindowOpen(exports.fgWindow);
 
@@ -168,5 +167,3 @@ function fireLifecycleEvents(manifestJson) {
     });
   });
 }
-
-require('cordova/channel').onPluginsReady.subscribe(boot);

plugin.xml
@@ -35,6 +35,7 @@
   <asset src="www/chromebgpage.html" target="plugins/org.chromium.bootstrap/chromebgpage.html" />
   <asset src="www/chromeappstyles.css" target="plugins/org.chromium.bootstrap/chromeappstyles.css" />
   <asset src="www/bgboot.js" target="plugins/org.chromium.bootstrap/bgboot.js" />
+  <asset src="www/fgboot.js" target="plugins/org.chromium.bootstrap/fgboot.js" />
 
   <platform name="android">
     <source-file src="src/android/ChromeAppWindow.java" target-dir="src/org/chromium" />

www/chromeapp.html
@@ -13,6 +13,7 @@ found in the LICENSE file.
         <script src="../../cordova.js"></script>
     </head>
     <body style="background-color: black;">
+        <script src="fgboot.js"></script>
         <script src="chrome-extension://z/chrome-content-loaded"></script>
     </body>
 </html>

www/fgboot.js
@@ -1,0 +1,6 @@
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+cordova.require('cordova/channel').onPluginsReady.subscribe(function() {
+    cordova.require('org.chromium.bootstrap.mobile.impl').fgInit();
+});